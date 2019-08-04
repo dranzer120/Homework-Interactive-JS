2ce9a7faa8ee1f5bb22fd3b0bdf2a1db504ab0be
Jason Tong <jasontong818@gmail.com>
2019-08-04 00:00:15 -0700

    second commit

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,13 +3,27 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(`/metadata/${sample}`).then((sampleData) => {
 
+  
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var selector = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
+    
+    selector.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(sampleData).forEach(([key, value]) => {
+      // Log the key and value
+      selector
+        .append("p")
+        .text(`${key} : ${value}`)
+        .property("value", key);
+    });
+
+  });
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
@@ -18,18 +32,39 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then((sampleData) => {
 
+  
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var slicedData = {};
+    
+    slicedData.otu_ids = sampleData.otu_ids.slice(0,10)
+    slicedData.otu_labels = sampleData.otu_labels.slice(0,10)
+    slicedData.sample_values = sampleData.sample_values.slice(0,10)
+
+    var data = [{
+      values: slicedData.sample_values,
+      labels: slicedData.otu_ids,
+      type: "pie"
+    }];
+  
+    var layout = {
+      height: 600,
+      width: 800
+    };
+  
+    Plotly.plot("pie", data, layout);
+
+  });
 }
 
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
@@ -41,6 +76,7 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
+
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
